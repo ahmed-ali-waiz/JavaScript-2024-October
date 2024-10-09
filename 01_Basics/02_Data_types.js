c58c88b421071e96a43cd792f896ca548d2674a0
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-10-09 18:16:16 +0000

    I commit this

--- 01_Basics/02_Data_types.js
+++ 01_Basics/02_Data_types.js
@@ -1,1 +1,15 @@
-console.log("Ali");
+// Int  2 to power 53
+// BigInt
+//String  ""
+//boolean true/false
+//Null   Standalone Value
+//Undefined  
+console.log(typeof 53);   //number
+
+console.log(typeof 53.2);  // Its also a number there are no float concepts in js
+
+console.log(typeof "ALi")  //string
+
+console.log(typeof null);    //object
+
+console.log(typeof undefined);  //undefined

9832f47f6eee43cdb3ceaf2238f60c1b8033f97c
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-10-09 19:25:20 +0000

    I commit this

--- 01_Basics/02_Data_types.js
+++ 01_Basics/02_Data_types.js
@@ -1,3 +1,4 @@
+// Primitives data type
 // Int  2 to power 53
 // BigInt
 //String  ""
@@ -13,3 +14,13 @@ console.log(typeof "ALi")  //string
 console.log(typeof null);    //object
 
 console.log(typeof undefined);  //undefined
+
+// Non primitive data type 
+
+// arrays, objects , Functions
+
+// Dynamically type language 
+// it means that we dont need to declare any type of variable in js like c++ or java
+
+let num = 10;   // number
+let num1 = "20" // string
---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,13 @@
+console.log(null>0);     //false
+console.log(null == 0)  //false
+console.log(null >=0)  //true
+
+// In js comparison operators  >= or <= and equality == work diffferently
+//In line 3 null converted to number and treated as 0 so it gives true
+
+// triple equal ===
+
+console.log("2"==2);  //true
+console.log("2"===2); //false
+
+//triple equal check strictly and it does not convert into num thats why it give false.

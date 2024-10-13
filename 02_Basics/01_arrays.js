989d36a0ea67bf3258ad535e71b241bc4aa13985
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-10-13 19:02:12 +0000

    I commit this

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,1 +1,35 @@
-console.log("Ali");
+let arr = [0,1,2,3,4,5];
+
+let students = ["Ahmad","Ali"];
+
+// const array = new Array(1,2,3,4);
+
+// console.log(array);
+console.log(arr[4]);
+
+// Array Methods
+
+arr.push(6,7);
+arr.pop()
+console.log(arr);
+
+console.log(arr.includes(6));
+console.log(arr.includes(7));
+console.log(arr.indexOf(4));
+
+// slice or splice
+
+const array = [1,2,3,4,5];
+
+const sliced = array.slice(0,4);// 5 is excluded but it does not change the original array
+
+console.log(sliced);
+console.log(array);  // original array remain same
+
+const arr2 = [1,2,3,4,5];
+
+const splice = arr2.splice(1,2);
+
+console.log(splice); // it prints 2,3 and remove it from original array
+
+console.log(arr2); // splice remove 1 and 2 index from original array

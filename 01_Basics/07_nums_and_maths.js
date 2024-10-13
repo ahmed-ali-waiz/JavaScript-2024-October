2e9aa3634ac730749799b794bb772fd049a430a6
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-10-13 15:11:39 +0000

    I commit this

--- 01_Basics/07_nums_and_maths.js
+++ 01_Basics/07_nums_and_maths.js
@@ -23,7 +23,7 @@ console.log(Math.pow(5,3));   //125
 console.log((Math.random()*10)+1);
 
 console.log(Math.floor((Math.random()*10)+1));
-const max = 10;
-const min = 20;
+const min = 10;
+const max = 20;
 
-console.log(Math.floor(Math.random()* max- min+1) + min)
+console.log(Math.floor(Math.random()* (max- min+1)) + min)

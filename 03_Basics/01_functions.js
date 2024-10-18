b221a02b8397f4fef71311c97e0721d3ffffb635
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-10-18 19:37:47 +0000

    I commit this

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,1 +1,15 @@
-console.log("ALo");
+function add(num1,num2) {
+    return ( num1+num2)
+}
+
+let a = add(4,8)
+console.log(a)  
+
+function message(username){
+    if(username === undefined){
+        console.log("please enter a username")
+    }
+    return  `${username} just logged in `
+}
+console.log(message("Ahmad"))   //Ahmad just logged in
+message()   // please enter a username

15195c908c1bff768c4da7bfd163064fffb79faa
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-10-20 10:57:11 +0000

    I commit this

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -13,3 +13,24 @@ function message(username){
 }
 console.log(message("Ahmad"))   //Ahmad just logged in
 message()   // please enter a username
+
+function calculateMart(...num1){
+    return num1
+}
+
+console.log(calculateMart(400,200,467))
+function calculateMartP(val1,val2, ...num1){
+    return num1
+}
+
+console.log(calculateMartP(400,200,467,399,700))
+
+const user = {
+    username : "Ahmad",
+    age : 20,
+}
+function objectsPassing(anyobject){
+    console.log(`username is ${anyobject.username} and age is ${anyobject.age}`)
+}
+
+handleo

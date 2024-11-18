fa00781de89323f4eb64e497595799d019b4e0f9
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-11-18 13:59:56 +0000

    I commit this

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,53 @@
+// if
+
+// if(2 == "2"){
+//     console.log("true");
+    
+// }
+
+// // if-else
+
+// if(2 === "2"){
+//     console.log("true");
+    
+// }
+// else{
+//     console.log("false");
+    
+// }
+// const score = 200;
+// let power = "fly";
+// let power2 = `invisible`;
+// if (score<100) {
+//     console.log(`power is ${power}`);
+    
+// } else {
+//     console.log(`power is ${power2}`);
+    
+// }
+
+                // logical operator
+ 
+    const ahmad = true;
+    const ali = false;
+    
+    if (ahmad&&ali) {
+        console.log("executed");
+        
+    }
+    else{
+        console.log(`not executed`);
+        
+    }
+
+    const LoggedInfromGoogle = true;
+    const LoggedInfromEmail = false;
+
+    if (LoggedInfromEmail || LoggedInfromGoogle) {
+        
+        console.log("user log in");
+        
+    } else {
+        console.log("user not logged in");
+        
+    }

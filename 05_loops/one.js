e165ea11384394da9f2c72a481b4f4576cd38f98
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2024-11-26 13:23:13 +0000

    I commit this

--- 05_loops/one.js
+++ 05_loops/one.js
@@ -17,14 +17,57 @@
     
 // }
 
-for (let index = 0; index < 10; index++) {
 
-    console.log(`Table of ${index} is :`);
 
-    for (let j = 1; j <= 10; j++) {
+// for (let index = 0; index < 10; index++) {
+
+//     console.log(`Table of ${index} is :`);
+
+//     for (let j = 1; j <= 10; j++) {
         
-        console.log(`${index} * ${j} = ${index*j}`);
+//         console.log(`${index} * ${j} = ${index*j}`);
         
-    }
+//     }
     
+// }
+
+// let arr = [`Ahmad`, 'Hassan', "Saad"];
+
+// console.log(arr.length);
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+
+//     console.log(element);
+    
+    
+// }
+
+
+// break and continue
+
+//break
+
+// for (let i = 1; i < 20; i++) {
+// if (i==6) {
+
+//     console.log(`Detected 6`);
+//     break;
+// }  
+
+// console.log(i);
+
+// }
+
+
+// continue
+for (let i = 1; i < 20; i++) {
+if (i==6) {
+
+    console.log(`Detected 6`);
+    continue;
+}  
+
+console.log(i);
+
 }
---
+++ 05_loops/two.js
@@ -0,0 +1,32 @@
+// while loop 
+
+
+// let a = 1;
+// let i =2;
+
+// while (a<= 10) {
+//     console.log(`${i} * ${a} = ${i*a}`);
+//     a++;
+// }
+
+
+
+// let arr = ["Ahmad ", "Hassan ", "Saad"];
+
+// let ar= 0;
+
+// while (ar< arr.length) {
+//     console.log(`value of arr is ${arr[ar]}`);
+//     ar++;
+// }
+
+let sco = 4;
+let score = 0;
+
+do {
+    console.log(`${sco} * ${score} = ${sco*score}`);
+    score++;
+    
+} while (score <= 10);
+
+// do-while loop can execute atleast one time even if the condition is wrong.

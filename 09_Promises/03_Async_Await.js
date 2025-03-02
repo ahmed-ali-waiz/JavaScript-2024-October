f43d0f88c8bba7a29c7a35988a924f0b9b1a35ad
Ahmad Ali <103488484+12248rer@users.noreply.github.com>
2025-03-02 13:03:07 +0000

    I commit this

--- 09_Promises/03_Async_Await.js
+++ 09_Promises/03_Async_Await.js
@@ -1,15 +1,15 @@
-async function Ahmad(){
-    let LahoreWeather = new Promise((resolve , reject)=>{
-        setTimeout(() => {
-            resolve("27 deg")
-        }, 5000);
-    })
+// async function Ahmad(){
+//     let LahoreWeather = new Promise((resolve , reject)=>{
+//         setTimeout(() => {
+//             resolve("27 deg")
+//         }, 5000);
+//     })
 
-    let IslWeather = new Promise((resolve , reject)=>{
-        setTimeout(() => {
-            resolve("28 deg")
-        }, 10000);
-    })
+//     let IslWeather = new Promise((resolve , reject)=>{
+//         setTimeout(() => {
+//             resolve("28 deg")
+//         }, 10000);
+//     })
 
      // Sequential Awaiting: When you await one promise after the other and then log both results together, 
     // the final log happens only after the last promise resolves.
@@ -29,17 +29,42 @@ async function Ahmad(){
     // console.log("Islamabad Weather:", isl);
 
 
-    console.log("Fetching lahore weather plz wait...");
-    let lahore = await LahoreWeather;  
-    console.log("Fetched lahore weather :" + lahore);
-    console.log("Fetching isl weather plz wait...");
-    let isl = await IslWeather;         
-   console.log("Fetched isl weather :" + isl);
+//     console.log("Fetching lahore weather plz wait...");
+//     let lahore = await LahoreWeather;  
+//     console.log("Fetched lahore weather :" + lahore);
+//     console.log("Fetching isl weather plz wait...");
+//     let isl = await IslWeather;         
+//    console.log("Fetched isl weather :" + isl);
    
-    return[lahore,isl]
-}
-let a = Ahmad()
-a.then((value)=>{
-    console.log(value);
+//     return[lahore,isl]
+// }
+// let a = Ahmad()
+// a.then((ali)=>{
+//     console.log(ali);
     
-})
+// })
+
+// Error handling
+
+setTimeout(() => {
+    console.log(`Getting data plz wait ...`)
+    
+}, 2000);
+
+try{
+    console.log(Ahmad);
+    
+}
+catch(error){
+  console.log("Shaka boom");
+  
+}
+setTimeout(() => {
+    console.log(`Fetching data from Ahmad plz wait ...`)
+
+}, 3000);
+
+setTimeout(() => {
+    console.log(`Hacking gmail  plz wait ...`)
+    
+}, 4000);
